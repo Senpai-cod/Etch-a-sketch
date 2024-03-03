7912b07ad1f347629e0f46aa6287414cbf7f0dcf
Senpai-cod <155851163+Senpai-cod@users.noreply.github.com>
2024-03-03 11:30:09 +0100

    Add an option for the user to enter the number of grid when clicking the button and Improve readability

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body");
+
 const div = document.createElement("div");
 div.classList.add("container");
 body.appendChild(div);
@@ -7,22 +8,59 @@ for(let j= 0; j< 16; j++){
     const rows = document.createElement("div")
     rows.classList.add("rows");
     div.appendChild(rows)
- for(let i=0; i < 16; i++){
-    const divs = document.createElement("div");
-    divs.classList.add("grid");
-    rows.appendChild(divs);
+        for(let i=0; i < 16; i++){
+            const divs = document.createElement("div");
+            divs.classList.add("grid");
+            rows.appendChild(divs);  
+}
+  }
 
-   
+const colorDivs = document.querySelectorAll(".grid");
+colorDivs.forEach((divs) =>{
+    divs.addEventListener("mouseover", ()=>{
+    divs.style.backgroundColor = "grey";
+});
+    divs.addEventListener("mouseout", ()=>{
+    divs.style.backgroundColor = "white";
+});
+    });
+
+const button = document.createElement("button");
+body.insertBefore(button,div);
+button.textContent = "Click me";
+
+button.addEventListener("click", ()=>{
+    let gridNum = 1;
+   do{
+    gridNum = prompt("Enter The number of squares per side for the new grid.");
+   }while(gridNum > 100 || gridNum < 1);
+
+    body.removeChild(div);
+    
+    const divSecond = document.createElement("div");
+    divSecond.classList.add("container");
+    body.appendChild(divSecond);
+
+for(let j= 0; j< Number(gridNum); j++){
+    const rows = document.createElement("div")
+    rows.classList.add("rows");
+    divSecond.appendChild(rows)
+    for(let i=0; i < Number(gridNum); i++){
+        const divs = document.createElement("div");
+        divs.classList.add("grid");
+        rows.appendChild(divs);
 }
 }
 const colorDivs = document.querySelectorAll(".grid");
 colorDivs.forEach((divs) =>{
-divs.addEventListener("mouseover", ()=>{
+    divs.addEventListener("mouseover", ()=>{
     divs.style.backgroundColor = "grey";
 
 });
-divs.addEventListener("mouseout", ()=>{
+    divs.addEventListener("mouseout", ()=>{
     divs.style.backgroundColor = "white";
+});
 
-});
-});
+    });
+
+        });
